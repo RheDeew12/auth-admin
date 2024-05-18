f2bd9e786cf0a37ced7510c338755a1a23d9f7c0
RheDeew12 <127844922+RheDeew12@users.noreply.github.com>
2024-05-18 15:51:32 +0700

    Update controllerAdmin.js

--- controllers/controllerAdmin.js
+++ controllers/controllerAdmin.js
@@ -7,23 +7,23 @@ exports.login = async (req, res) => {
 
   Admin.findById(username, async (err, data) => {
     if (err) {
-      return res.status(500).json({ message: "Error fetching data" });
+      return res.status(500).send({ message: "Error fetching data" });
     }
 
     if (!data || data.length === 0) {
-      return res.status(404).json({ message: "Admin not found" });
+      return res.status(404).send({ message: "Admin not found" });
     }
 
     try {
-      const passwordMatch = await bcrypt.compare(password, data[0].password);
-      if (passwordMatch) {
+      const isMatch = await bcrypt.compare(password, data[0].password);
+      if (isMatch) {
         const token = jwt.sign({ username: data[0].username }, 'secretkey');
-        return res.status(200).json({ message: "Login successful", token });
+        return res.status(200).send({ message: "Login successful", token });
       } else {
-        return res.status(401).json({ message: "Incorrect password" });
+        return res.status(401).send({ message: "Incorrect password" });
       }
     } catch (error) {
-      return res.status(500).json({ message: "Authentication error" });
+      return res.status(500).send({ message: "Error during authentication" });
     }
   });
 };
@@ -31,15 +31,15 @@ exports.login = async (req, res) => {
 exports.findById = (req, res) => {
   Admin.findById(req.params.username, (err, data) => {
     if (err) {
-      return res.status(500).json({ message: "Error fetching data" });
+      res.status(500).send({ message: "Error fetching data" });
+    } else {
+      res.json(data);
     }
-    res.json(data);
   });
 };
 
 exports.create = async (req, res) => {
   const newAdmin = new Admin(req.body);
-
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const newAdmin = new Admin({
@@ -47,35 +47,38 @@ exports.create = async (req, res) => {
       password: hashedPassword,
     });
 
-    if (Object.keys(req.body).length === 0) {
-      return res.status(400).json({ message: "Please provide all required fields" });
-    } else {
-      Admin.create(newAdmin, (err, data) => {
-        if (err) {
-          return res.status(500).json({ message: "Error creating data" });
-        }
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).send({ message: "Please provide all required fields" });
+  } else {
+    Admin.create(newAdmin, (err, data) => {
+      if (err) {
+        res.status(500).send({ message: "Error creating data" });
+      } else {
         res.json({ message: "Admin added successfully", data });
-      });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Error creating data" });
+      }
+    });
   }
+} catch (error) {
+  res.status(500).send({ message: "Error creating data" });
+}
 };
 
 exports.findAll = (req, res) => {
   Admin.findAll((err, data) => {
     if (err) {
-      return res.status(500).json({ message: "Error fetching data" });
+      res.status(500).send({ message: "Error fetching data" });
+    } else {
+      res.send(data);
     }
-    res.send(data);
   });
 };
 
 exports.delete = (req, res) => {
   Admin.delete(req.params.username, (err, data) => {
     if (err) {
-      return res.status(500).json({ message: "Error deleting data" });
+      res.status(500).send({ message: "Error deleting data" });
+    } else {
+      res.json({ message: "Admin deleted successfully" });
     }
-    res.json({ message: "Admin deleted successfully" });
   });
 };
