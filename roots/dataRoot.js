8ae1340067f700f14163bb76f96ff73591728c64
RheDeew12 <127844922+RheDeew12@users.noreply.github.com>
2024-05-18 14:29:18 +0700

    Update dataRoot.js

--- roots/dataRoot.js
+++ roots/dataRoot.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/controllerAdmin');
-const authHandler = require("../handler/authHandler");
 
 router.get("/admin/:username", adminController.findById);
 router.post("/admin", adminController.create);
-router.post("/admin/login", authHandler.login);
+router.post("/admin/login", adminController.login);
 router.get("/admin", adminController.findAll);
 router.delete("/admin/:username", adminController.delete);
 
