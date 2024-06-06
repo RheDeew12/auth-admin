4a64d3212c6062a3e7aa3ed7fcbbae9e9833024f
naufalfizbar <91106131+naufalfizbar@users.noreply.github.com>
2024-06-06 09:57:14 +0700

    Update dataRoot.js

--- roots/dataRoot.js
+++ roots/dataRoot.js
@@ -8,4 +8,6 @@ router.post("/admin/login", adminController.login);
 router.get("/admin", adminController.findAll);
 router.delete("/admin/:username", adminController.delete);
 
+router.get("/admin/check-username", adminController.checkUsername);
+
 module.exports = router;
